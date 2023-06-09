ebb9c24ba70c717e5bbdbe30217ff6506e5be804
Emmy Bishop <miaui96@gmail.com>
2023-06-09 13:19:40 -0500

    Add app.listen and npm start script

--- server/index.ts
+++ server/index.ts
@@ -3,4 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { PORT } = process.env;
 
-console.log(PORT);
+const app = Express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://127.0.0.1:${PORT}`);
+})
