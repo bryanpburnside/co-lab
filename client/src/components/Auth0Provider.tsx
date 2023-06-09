6ee564b085ea7d55720fee29c1067da660dcefad
Emmy Bishop <miaui96@gmail.com>
2023-06-09 17:43:41 -0500

    Add Auth0Provider component

---
+++ client/src/components/Auth0Provider.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+
+const { DOMAIN, CLIENT_ID } = process.env;
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <Auth0Provider
+    domain={DOMAIN || ''}
+    clientId={CLIENT_ID || ''}
+    authorizationParams={{
+      redirect_uri: window.location.origin // change once homepage is created
+    }}
+  >
+    <App />
+  </Auth0Provider>,
+);
