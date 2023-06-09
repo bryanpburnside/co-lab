223b0cbcb42267bb6cb5f4108faf84928de767a9
Emmy Bishop <miaui96@gmail.com>
2023-06-08 19:35:25 -0500

    Add eslint, webpack, tsconfig, babel

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+interface Props {
+  name:
+  string
+}
+
+const App = () => {
+  return (
+    <>
+      <h1>
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+import './styles.css';
+
+const app = document.getElementById('app') as HTMLElement;
+const root = createRoot(app);
+root.render(<App name="app" />);
