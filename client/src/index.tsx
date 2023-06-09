25e26e3d5bd9a4c4d0d095715700bbe2a2eb4c70
Emmy Bishop <miaui96@gmail.com>
2023-06-09 17:41:43 -0500

    Fix TS error by removing file extension from import

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import App from './components/App';
 import './styles.css';
 
 const app = document.getElementById('app') as HTMLElement;
