885f227c8b595b5795f3bed98205198a3014b15a
Emmy Bishop <miaui96@gmail.com>
2023-06-10 13:21:47 -0500

    Reconfigure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,10 @@ const config = {
   entry: './client/src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
+  watch: true,
   module: {
     rules: [
       {
@@ -39,13 +41,14 @@ const config = {
     new HtmlWebpackPlugin({
       templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
       filename: 'index.html',
-    })
+    }),
   ],
   resolve: {
     extensions: [
       '.tsx',
       '.ts',
-      '.js'
+      '.js',
+      '.jsx'
     ]
   }
 };
