320dc0a72c02263f3520b4952e5ec9c50b850bdf
Emmy Bishop <miaui96@gmail.com>
2023-06-10 14:30:26 -0500

    Move Auth0Provider into index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import App from './components/App';
 import './styles.css';
 
+const { DOMAIN, CLIENT_ID } = process.env;
+
 const app = document.getElementById('app') as HTMLElement;
 const root = createRoot(app);
-root.render(<App />);
+
+root.render(<Auth0Provider
+  domain={DOMAIN || ''}
+  clientId={CLIENT_ID || ''}
+  authorizationParams={{
+    redirect_uri: window.location.origin // change once homepage is created
+  }}
+>
+  <App />
+</Auth0Provider>,);
