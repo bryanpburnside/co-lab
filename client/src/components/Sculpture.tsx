f0973353b8ed58d7d8ddc5cabe84d4d2812e3734
Bryan Burnside <bpburnside@gmail.com>
2023-06-12 17:40:07 -0500

    Add peerjs, grab roomId with useParams hook, and create peer

--- client/src/components/Sculpture.tsx
+++ client/src/components/Sculpture.tsx
@@ -1,8 +1,17 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import Peer from 'peerjs';
+import { useParams } from 'react-router-dom'
 
 const Sculpture = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (user) {
+    const peer = new Peer(user.sub as string, {
+      host: '/',
+      port: 3001,
+    });
+  }
 
   return (
       <div>
