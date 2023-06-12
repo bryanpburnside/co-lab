17aa0cdfdc910925636f93ddb296cf4e66d850fe
Emmy Bishop <miaui96@gmail.com>
2023-06-12 14:20:30 -0500

    Add function to grab user info after login redirect

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import Navbar from './Navbar';
 import Home from './Home';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import Profile from './Profile';
-
-// interface Props {
-//   name:
-//   string
-// }
+import saveUser from './AuthService';
 
 const App = () => {
-  return (
+  const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
 
+  const getUserInfo = async () => {
+    try {
+      console.log(user);
+      saveUser();
+    } catch (err) {
+      console.error('Failed to GET active user info at client:', err);
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, [isAuthenticated, user])
+
+  return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<LoginButton />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/logout' element={<LogoutButton />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/logout" element={<LogoutButton />} />
+        <Route path="/login/*" element={<LoginButton />} />
       </Routes>
     </Router>
   );
