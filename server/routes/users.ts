f266f7fbe370a95013f43910b75081f4261c54cf
Emmy Bishop <miaui96@gmail.com>
2023-06-12 15:35:05 -0500

    Delete unused code

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -4,7 +4,6 @@ import { User } from '../database/index.js';
 
 Users.post('/', async (req, res) => {
   const { id, name, email } = req.body;
-  console.log(id, name, email);
   try {
     const newUser = await User.create({ id, name, email });
     if (newUser) {
