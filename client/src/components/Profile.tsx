f869cc334f0b2cfe8e664f55e2022409a3148fa5
Emmy Bishop <miaui96@gmail.com>
2023-06-12 17:13:17 -0500

    Fix TS error by checking if user exists before rendering profile

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -9,7 +9,7 @@ const Profile = () => {
   }
 
   return (
-    isAuthenticated ? (
+    user && isAuthenticated ? (
       <div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
