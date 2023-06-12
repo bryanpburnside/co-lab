9d5dcfd97792fd89ba740413837751a9e61a6308
Emmy Bishop <miaui96@gmail.com>
2023-06-12 14:36:52 -0500

    Move function to grab user info to App

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import Navbar from './Navbar';
@@ -6,15 +7,27 @@ import Home from './Home';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import Profile from './Profile';
-import saveUser from './AuthService';
 
 const App = () => {
   const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
 
+  const saveUser = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      console.log('User ID:', user.sub);
+      console.log('Email:', user.email);
+      console.log('Name:', user.name);
+      console.log('Access Token:', token);
+      console.log('user saved!');
+    } catch (err) {
+      console.error('Failed to SAVE user to db at client:', err);
+    }
+  }
+
   const getUserInfo = async () => {
     try {
       console.log(user);
-      saveUser();
+      await saveUser();
     } catch (err) {
       console.error('Failed to GET active user info at client:', err);
     }
