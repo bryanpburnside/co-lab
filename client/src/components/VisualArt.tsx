6b92a0ce1f749aafd32a310b4b7b798a46b4af29
Emmy Bishop <miaui96@gmail.com>
2023-06-13 13:51:46 -0500

    Add basic VisualArt component. Currently allows drawing + saving drawing to db

---
+++ client/src/components/VisualArt.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useRef } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import paper, { Color } from 'paper';
+
+const VisualArt = () => {
+  const { user } = useAuth0();
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    paper.setup(canvas);
+    const tool = new paper.Tool();
+    let path: paper.Path | null;
+
+    tool.onMouseDown = (event: paper.ToolEvent) => {
+      path = new paper.Path();
+      path.strokeColor = new (Color as typeof Color)('black');
+      path.add(event.point);
+    };
+
+    tool.onMouseDrag = (event: paper.ToolEvent) => {
+      if (path) {
+        path.add(event.point);
+      }
+    };
+
+    tool.onMouseUp = () => {
+      path = null;
+    };
+
+    const resizeCanvas = () => {
+      paper.view.viewSize = new paper.Size(canvas.clientWidth, canvas.clientHeight);
+    };
+
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
+
+    return () => {
+      window.removeEventListener('resize', resizeCanvas);
+    };
+  }, []);
+
+  const saveArt = async (art: string) => {
+    try {
+      await axios.post('/visualart', { art, user });
+    } catch (err) {
+      console.error('Failed to SAVE art to db at client:', err);
+    }
+  }
+
+  const handleSaveClick = async () => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const art = canvas.toDataURL();
+
+    await saveArt(art);
+  };
+
+  return (
+    <>
+      <canvas
+        ref={canvasRef}
+        style={{ width: '100vw', height: '100vh' }}
+      />
+      <button type="submit" onClick={handleSaveClick}>Save</button>
+    </>
+  );
+}
+
+export default VisualArt;
