9477042773053db76eab5f22ecaf25325ac07c18
Bryan Burnside <bpburnside@gmail.com>
2023-06-15 13:32:13 -0500

    Update Sculpture component socket.io logic in useEffect

--- client/src/components/Sculpture.tsx
+++ client/src/components/Sculpture.tsx
@@ -1,22 +1,40 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
-import Peer from 'peerjs';
+import React, { useState, useEffect } from "react";
+// import Peer from 'peerjs';
 import { useParams } from 'react-router-dom'
+import { io } from 'socket.io-client';
 
 const Sculpture = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+  const { roomId } = useParams();
+  const socket = io('http://localhost:8000');
 
-  if (user) {
-    const peer = new Peer(user.sub as string, {
-      host: '/',
-      port: 3001,
+
+  useEffect(() => {
+    socket.on('roomCreated', (userId, roomId) => {
+      console.log(`${userId} created room: ${roomId}`);
     });
-  }
+
+    socket.on('userJoined', (userId) => {
+      socket.emit('logJoinUser', userId);
+      console.log(`User ${userId} joined the room`);
+    });
+
+    socket.on('userLeft', (userId) => {
+      console.log(`User ${userId} left the room`);
+    });
+
+    // Clean up the socket.io connection when the component unmounts
+    return () => {
+      socket.emit('disconnectUser', user?.sub);
+      socket.disconnect();
+    };
+  }, [roomId]);
 
   return (
-      <div>
-        
-      </div>
+    <div>
+
+    </div>
   );
 };
 
