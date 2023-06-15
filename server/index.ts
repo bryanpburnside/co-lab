f276e5019daf9146ee6159bee148074329954298
Bryan Burnside <bpburnside@gmail.com>
2023-06-15 13:29:01 -0500

    Add socket.io logic to server index

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,29 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+import { sequelize, initialize } from './database/index.js';
+// import Rooms from './routes/rooms.js';
+import { v4 as generateRoomId } from 'uuid';
+
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+import cors from 'cors';
 dotenv.config({ path: path.resolve(dirname(fileURLToPath(import.meta.url)), '../.env') });
 const { PORT } = process.env;
-import { sequelize, initialize } from './database/index.js';
-import Login from './routes/login.js';
 
 const app = express();
+const Rooms: Router = express.Router();
+
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:8000',
+    methods: ['GET', 'POST'],
+  }
+});
+
 const currentDirectory = path.dirname(fileURLToPath(import.meta.url));
 const staticFilesPath = path.join(currentDirectory, '../dist');
 
@@ -15,8 +31,7 @@ const staticFilesPath = path.join(currentDirectory, '../dist');
 app.use(express.json());
 
 // ROUTES
-// app.use('/login', Login);
-
+app.use('/api/rooms', Rooms);
 app.use(express.static(staticFilesPath));
 
 app.get('*', (req, res) => {
@@ -34,6 +49,37 @@ sequelize.authenticate()
 
 initialize();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://127.0.0.1:${PORT}`);
+Rooms.post('/', (req, res) => {
+  const { userId } = req.body;
+  const roomId = generateRoomId();
+
+  res.json({ userId, roomId });
 })
+
+io.on('connection', socket => {
+  // Handle create room event
+  socket.on('createRoom', (userId, roomId) => {
+    socket.join(roomId); // Join the room with the generated ID
+    socket.emit('roomCreated', userId, roomId); // Emit the roomCreated event to the user
+    console.log(`${userId} created room: ${roomId}`);
+  });
+
+  // Handle join room event
+  socket.on('joinRoom', (userId, roomId) => {
+    socket.join(roomId); // Join the room with the provided ID
+    socket.to(roomId).emit('userJoined', userId); // Emit the userJoined event to the participants in the room
+    console.log(`User ${userId} joined the room`);
+  });
+
+  socket.on('logJoinUser', (userId) => {
+    console.log(`User ${userId} joined the room`);
+  });
+
+  socket.on('disconnectUser', userId => {
+    console.log(`${userId} left the room`);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on http://127.0.0.1:${PORT}`);
+});
