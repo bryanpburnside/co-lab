16347092b6ba794cdb8a78acd3905ce9fbe9ca09
Emmy Bishop <miaui96@gmail.com>
2023-06-16 13:16:00 -0500

    Update VisualArt to set bg color of canvas and pass color down as prop to each mode

--- client/src/components/VisualArt.tsx
+++ client/src/components/VisualArt.tsx
@@ -11,12 +11,12 @@ enum ActiveComponent {
 }
 
 const VisualArt: React.FC = () => {
-  const { DrawMode, PatternMode } = ActiveComponent
-  const [mode, setMode] = useState<ActiveComponent>(DrawMode);
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user } = useAuth0();
   const { roomId } = useParams();
   const socket = io('http://localhost:8000');
-
+  const { DrawMode, PatternMode } = ActiveComponent;
+  const [mode, setMode] = useState<ActiveComponent>(DrawMode);
+  const [backgroundColor, setBackgroundColor] = useState('#3d3983');
 
   useEffect(() => {
     socket.on('roomCreated', (userId, roomId) => {
@@ -39,17 +39,23 @@ const VisualArt: React.FC = () => {
     };
   }, [roomId]);
 
+  const handleBackgroundColorChange = (e) => {
+    const { value } = e.target;
+    setBackgroundColor(value);
+  };
+
   const renderComponent = () => {
     switch (mode) {
       case PatternMode:
-        return <RandomPattern />;
+        return <RandomPattern backgroundColor={backgroundColor} handleBackgroundColorChange={handleBackgroundColorChange} />;
       case DrawMode:
-        return <Draw />;
+        return <Draw backgroundColor={backgroundColor} handleBackgroundColorChange={handleBackgroundColorChange} />;
     }
   }
 
   return (
     <div>
+      <p>Select a Mode:</p>
       <button onClick={() => setMode(DrawMode)}>Drawing</button>
       <button onClick={() => setMode(PatternMode)}>Pattern</button>
       {renderComponent()}
@@ -58,3 +64,64 @@ const VisualArt: React.FC = () => {
 }
 
 export default VisualArt;
+
+
+// import { useAuth0 } from "@auth0/auth0-react";
+// import React, { useState, useEffect } from 'react';
+// import Draw from './Draw';
+// import RandomPattern from './RandomPattern';
+// import { useParams } from 'react-router-dom'
+// import { io } from 'socket.io-client';
+
+// enum ActiveComponent {
+//   DrawMode,
+//   PatternMode
+// }
+
+// const VisualArt: React.FC = () => {
+//   const { DrawMode, PatternMode } = ActiveComponent
+//   const [mode, setMode] = useState<ActiveComponent>(DrawMode);
+//   const { user, isAuthenticated, isLoading } = useAuth0();
+//   const { roomId } = useParams();
+//   const socket = io('http://localhost:8000');
+
+//   useEffect(() => {
+//     socket.on('roomCreated', (userId, roomId) => {
+//       console.log(`${userId} created room: ${roomId}`);
+//     });
+
+//     socket.on('userJoined', (userId) => {
+//       socket.emit('logJoinUser', userId);
+//       console.log(`User ${userId} joined the room`);
+//     });
+
+//     socket.on('userLeft', (userId) => {
+//       console.log(`User ${userId} left the room`);
+//     });
+
+//     // Clean up the socket.io connection when the component unmounts
+//     return () => {
+//       socket.emit('disconnectUser', user?.sub);
+//       socket.disconnect();
+//     };
+//   }, [roomId]);
+
+//   const renderComponent = () => {
+//     switch (mode) {
+//       case PatternMode:
+//         return <RandomPattern />;
+//       case DrawMode:
+//         return <Draw />;
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={() => setMode(DrawMode)}>Drawing</button>
+//       <button onClick={() => setMode(PatternMode)}>Pattern</button>
+//       {renderComponent()}
+//     </div>
+//   )
+// }
+
+// export default VisualArt;
