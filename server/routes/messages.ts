abc7009af4d0ddd610e78164ccb4da9fbb55854c
Emmy Bishop <miaui96@gmail.com>
2023-06-21 13:45:04 -0500

    Update Messages get request to include names of sender and receiver in response

--- server/routes/messages.ts
+++ server/routes/messages.ts
@@ -1,12 +1,18 @@
 import { Op } from 'sequelize';
 import { Router } from 'express';
 const Messages = Router();
-import { Message } from '../database/index.js';
+import { Message, User } from '../database/index.js';
 
 Messages.get('/:userId', async (req, res) => {
   const { userId } = req.params;
   try {
-    const thread = await Message.findAll({ where: { [Op.or]: [{ senderId: userId }, { receiverId: userId }] } });
+    const thread = await Message.findAll({
+      where: { [Op.or]: [{ senderId: userId }, { receiverId: userId }] },
+      include: [
+        { model: User, as: 'sender', attributes: ['name'] },
+        { model: User, as: 'receiver', attributes: ['name'] },
+      ],
+    });
     if (thread) {
       res.status(200).send(thread);
     }
@@ -14,7 +20,28 @@ Messages.get('/:userId', async (req, res) => {
     console.error('Failed to GET messages from db:', err);
     res.sendStatus(500);
   }
-})
+});
 
 export default Messages;
+
+
+// import { Op } from 'sequelize';
+// import { Router } from 'express';
+// const Messages = Router();
+// import { Message } from '../database/index.js';
+
+// Messages.get('/:userId', async (req, res) => {
+//   const { userId } = req.params;
+//   try {
+//     const thread = await Message.findAll({ where: { [Op.or]: [{ senderId: userId }, { receiverId: userId }] } });
+//     if (thread) {
+//       res.status(200).send(thread);
+//     }
+//   } catch (err) {
+//     console.error('Failed to GET messages from db:', err);
+//     res.sendStatus(500);
+//   }
+// })
+
+// export default Messages;
 
