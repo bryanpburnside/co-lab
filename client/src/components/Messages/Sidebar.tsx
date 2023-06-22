5c96298115d87a4a51166f6858f56140cfbff36c
Emmy Bishop <miaui96@gmail.com>
2023-06-22 15:47:50 -0500

    Update Sidebar, place divs on page using styled-components

--- client/src/components/Messages/Sidebar.tsx
+++ client/src/components/Messages/Sidebar.tsx
@@ -1,30 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import { Socket } from 'socket.io-client';
+import { SocketContext } from './Inbox';
+import Messages from './Messaging';
+import Thread from './Thread';
+import styled from 'styled-components';
+
+const SidebarContainer = styled.div`
+  display: flex;
+`;
+
+const MessageList = styled.div`
+  flex: 1;
+  width: 25%;
+  padding: 20px;
+`;
+
+const ThreadContainer = styled.div`
+  flex: 3;
+  width: 75%;
+  padding: 20px;
+`;
 
 const Sidebar = () => {
+  const socket = useContext(SocketContext) as Socket;
+
+  const { user } = useAuth0();
+  const [userId, setUserId] = useState<string>('');
+  const [userList, setUserList] = useState<{ id: string; name: string }[]>([]);
+  const [recipient, setRecipient] = useState<string>('');
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get('/users');
+      setUserList(response.data);
+    } catch (err) {
+      console.error('Failed to GET user list:', err);
+    }
+  }
+
+  useEffect(() => {
+    setUserId(user?.sub);
+  }, [user])
+
+  useEffect(() => {
+    if (userId) {
+      getUsers();
+    }
+  }, [userId])
+
   return (
-    <div>sidebar</div>
+    <SidebarContainer>
+      <MessageList>
+        <h1>Threads</h1>
+        <ul>
+          {userList.map((user) => (
+            <li key={user.id}>
+              {user.name}
+            </li>
+          ))}
+        </ul>
+      </MessageList>
+      <ThreadContainer>
+        <Thread userId={userId} recipient={recipient} />
+      </ThreadContainer>
+    </SidebarContainer>
   )
 }
 
 export default Sidebar;
 
-
-
-
-
-// import React from 'react';
-// import Threads from './Threads';
-
-// const Sidebar = () => {
-//   return (
-//     <div style={{ width: '250px' }} className="sidebar">
-//       <h1>im the sidebar babey</h1>
-//       <Threads />
-//       <div>
-//         <button>New Message</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Sidebar;
--- client/src/components/Messages/Threads.tsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-
-// const Threads = () => {
-//   return (
-//     <div>threads threads threaaads</div>
-//   )
-// }
-
-// export default Threads;
