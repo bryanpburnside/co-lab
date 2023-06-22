d31896ef0f9711b570bc493d69c09e8d35968ea3
Emmy Bishop <miaui96@gmail.com>
2023-06-22 17:50:04 -0500

    Update server-side GET request to fetch all messages between current user and selected recipient

--- server/routes/messages.ts
+++ server/routes/messages.ts
@@ -3,16 +3,31 @@ import { Router } from 'express';
 const Messages = Router();
 import { Message, User } from '../database/index.js';
 
-Messages.get('/:userId', async (req, res) => {
-  const { userId } = req.params;
+Messages.get('/:userId/:recipientId', async (req, res) => {
+  const { userId, recipientId } = req.params;
   try {
     const thread = await Message.findAll({
-      where: { [Op.or]: [{ senderId: userId }, { receiverId: userId }] },
+      where: {
+        [Op.or]: [
+          {
+            [Op.and]: [
+              { senderId: userId },
+              { receiverId: recipientId },
+            ],
+          },
+          {
+            [Op.and]: [
+              { senderId: recipientId },
+              { receiverId: userId },
+            ],
+          },
+        ],
+      },
       include: [
         { model: User, as: 'sender', attributes: ['name'] },
         { model: User, as: 'receiver', attributes: ['name'] },
       ],
-      order: [['timestamp', 'ASC']]
+      order: [['timestamp', 'ASC']],
     });
     if (thread) {
       res.status(200).send(thread);
@@ -24,25 +39,3 @@ Messages.get('/:userId', async (req, res) => {
 });
 
 export default Messages;
-
-
-// import { Op } from 'sequelize';
-// import { Router } from 'express';
-// const Messages = Router();
-// import { Message } from '../database/index.js';
-
-// Messages.get('/:userId', async (req, res) => {
-//   const { userId } = req.params;
-//   try {
-//     const thread = await Message.findAll({ where: { [Op.or]: [{ senderId: userId }, { receiverId: userId }] } });
-//     if (thread) {
-//       res.status(200).send(thread);
-//     }
-//   } catch (err) {
-//     console.error('Failed to GET messages from db:', err);
-//     res.sendStatus(500);
-//   }
-// })
-
-// export default Messages;
-
