65095a35ca2c48d21b2784c2d4a7679cb69fbe7c
Emmy Bishop <miaui96@gmail.com>
2023-06-22 15:46:25 -0500

    Update imports

--- client/src/components/Messages/Inbox.tsx
+++ client/src/components/Messages/Inbox.tsx
@@ -1,21 +1,19 @@
 import React, { createContext } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import io, { Socket } from 'socket.io-client';
-import Sidebar from './Sidebar';
 import Messages from './Messaging';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
 
 export const SocketContext = createContext<Socket | null>(null);
-
 export const msgSocket = io('/');
 
 const Inbox: React.FC = () => {
-
   return (
     <SocketContext.Provider value={msgSocket}>
       <div className='body-container' style={{ position: 'relative' }}>
         <Sidebar />
-        <Messages />
-        <p>inbox</p>
+        <Footer />
       </div>
     </SocketContext.Provider>
   )
