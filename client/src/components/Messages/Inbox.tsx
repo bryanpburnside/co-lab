fedecf5af03c6982fba1f35b5cf26fffc262c219
Emmy Bishop <miaui96@gmail.com>
2023-06-22 16:01:07 -0500

    Add styling to message bubbles

--- client/src/components/Messages/Inbox.tsx
+++ client/src/components/Messages/Inbox.tsx
@@ -4,6 +4,16 @@ import io, { Socket } from 'socket.io-client';
 import Messages from './Messaging';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
+import styled from 'styled-components';
+
+const BodyContainer = styled.div`
+  background-color: #3d3983;
+  display: flex;
+  justify-content: space-between;
+  width: 80vw;
+  margin: 0 auto;
+  height: 60vh;
+`;
 
 export const SocketContext = createContext<Socket | null>(null);
 export const msgSocket = io('/');
@@ -11,10 +21,10 @@ export const msgSocket = io('/');
 const Inbox: React.FC = () => {
   return (
     <SocketContext.Provider value={msgSocket}>
-      <div className='body-container' style={{ position: 'relative' }}>
+      <BodyContainer>
         <Sidebar />
         <Footer />
-      </div>
+      </BodyContainer>
     </SocketContext.Provider>
   )
 }
--- client/src/components/Messages/Thread.tsx
+++ client/src/components/Messages/Thread.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 interface Message {
   id: number;
@@ -10,9 +11,34 @@ interface Message {
   };
 }
 
+const ConversationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow-y: auto;
+`;
+
+const SenderBubble = styled.div`
+  align-self: flex-start;
+  background-color: #e5e5ea;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
+const RecipientBubble = styled.div`
+  align-self: flex-end;
+  background-color: #007bff;
+  color: #fff;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
 const Thread = ({ userId, recipient }) => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [message, setMessage] = useState<string>('');
+  const conversationContainerRef = useRef<HTMLDivElement>(null);
 
   const getMessages = async () => {
     try {
@@ -22,26 +48,164 @@ const Thread = ({ userId, recipient }) => {
     } catch (err) {
       console.error('Failed to GET messages to client:', err);
     }
-  }
+  };
 
   useEffect(() => {
     if (userId) {
       getMessages();
     }
-  }, [userId])
+  }, [userId]);
+
+  useEffect(() => {
+    // Scroll to the bottom of the conversation container when messages change
+    if (conversationContainerRef.current) {
+      conversationContainerRef.current.scrollTop = conversationContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   return (
     <div>
       {userId}
-      <div>
+      <ConversationContainer ref={conversationContainerRef}>
         {messages.map((msg) => (
           <div key={msg.id}>
-            <span>{msg.sender.name}:</span> {msg.message}
+            {msg.senderId === userId ? (
+              <SenderBubble>{msg.message}</SenderBubble>
+            ) : (
+              <RecipientBubble>{msg.message}</RecipientBubble>
+            )}
           </div>
         ))}
-      </div>
+      </ConversationContainer>
     </div>
-  )
-}
+  );
+};
 
 export default Thread;
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import styled from 'styled-components';
+
+// interface Message {
+//   id: number;
+//   senderId: string;
+//   message: string;
+//   sender: {
+//     name: string;
+//   };
+// }
+
+// const ConversationContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+// `;
+
+// const SenderBubble = styled.div`
+//   align-self: flex-start;
+//   background-color: #e5e5ea;
+//   border-radius: 10px;
+//   padding: 10px;
+//   margin-bottom: 10px;
+// `;
+
+// const RecipientBubble = styled.div`
+//   align-self: flex-end;
+//   background-color: #007bff;
+//   color: #fff;
+//   border-radius: 10px;
+//   padding: 10px;
+//   margin-bottom: 10px;
+// `;
+
+// const Thread = ({ userId, recipient }) => {
+//   const [messages, setMessages] = useState<Message[]>([]);
+//   const [message, setMessage] = useState<string>('');
+
+//   const getMessages = async () => {
+//     try {
+//       const thread = await axios.get(`/messages/${userId}`);
+//       console.log(thread.data);
+//       setMessages(thread.data);
+//     } catch (err) {
+//       console.error('Failed to GET messages to client:', err);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (userId) {
+//       getMessages();
+//     }
+//   }, [userId]);
+
+//   return (
+//     <div>
+//       {userId}
+//       <ConversationContainer>
+//         {messages.map((msg) => (
+//           <div key={msg.id}>
+//             {msg.senderId === userId ? (
+//               <SenderBubble>{msg.message}</SenderBubble>
+//             ) : (
+//               <RecipientBubble>{msg.message}</RecipientBubble>
+//             )}
+//           </div>
+//         ))}
+//       </ConversationContainer>
+//     </div>
+//   );
+// };
+
+// export default Thread;
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// interface Message {
+//   id: number;
+//   senderId: string;
+//   message: string;
+//   sender: {
+//     name: string;
+//   };
+// }
+
+// const Thread = ({ userId, recipient }) => {
+//   const [messages, setMessages] = useState<Message[]>([]);
+//   const [message, setMessage] = useState<string>('');
+
+//   const getMessages = async () => {
+//     try {
+//       const thread = await axios.get(`/messages/${userId}`);
+//       console.log(thread.data);
+//       setMessages(thread.data);
+//     } catch (err) {
+//       console.error('Failed to GET messages to client:', err);
+//     }
+//   }
+
+//   useEffect(() => {
+//     if (userId) {
+//       getMessages();
+//     }
+//   }, [userId])
+
+//   return (
+//     <div>
+//       {userId}
+//       <div>
+//         {messages.map((msg) => (
+//           <div key={msg.id}>
+//             <span>{msg.sender.name}:</span> {msg.message}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Thread;
