79b3b915766b6accdb6579920bb762bf0de465fb
Emmy Bishop <miaui96@gmail.com>
2023-06-24 15:40:37 -0500

    Add 'add friend' button to profile

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -8,12 +9,24 @@ const Profile = () => {
     return <div className='loading-container'>Loading ...</div>;
   }
 
+  const addFriend = (id: any) => {
+    try {
+      axios.post('/users/add-friend', {
+        id,
+        friendId: 'google-oauth2|118010226608314597957'
+      })
+    } catch (err) {
+      console.error('Failed to POST new friend at client:', err);
+    }
+  }
+
   return (
     user && isAuthenticated ? (
       <div className='profile-container'>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+        <button onClick={() => addFriend(user.sub)}>Add Friend</button>
       </div>
     ) :
       (
