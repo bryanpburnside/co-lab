65d3ce58c0c11f5f83052f9b59b5bfed59fa96c8
Cynthia <ladypenguin1991@gmail.com>
2023-06-29 16:18:57 -0500

    seed data added

--- server/database/index.ts
+++ server/database/index.ts
@@ -1,5 +1,6 @@
 import { Sequelize, DataTypes, Model } from 'sequelize';
 const { DB_NAME, DB_USER, DB_PW } = process.env;
+import createSeedData from '../seeds/storySeeds.js';
 
 const sequelize = new Sequelize(DB_NAME || 'colab', DB_USER as string, DB_PW as string, {
   host: 'localhost',
@@ -197,6 +198,7 @@ Pages.belongsTo(Story, { foreignKey: 'storyId' })
 const initialize = async () => {
   try {
     await sequelize.sync({ force: true });
+    await createSeedData();
     console.log('Tables successfully created!');
   } catch (error) {
     console.error('Error creating tables :(', error);
