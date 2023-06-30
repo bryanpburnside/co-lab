971ad311c6c9a782348087e843bcdecb7b93fcae
Emmy Bishop <miaui96@gmail.com>
2023-06-30 14:20:07 -0500

    Add basic route to grab all user's artwork

--- server/routes/artwork.ts
+++ server/routes/artwork.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-const artworkRouter = Router();
 import { Artwork, User } from '../database/index.js';
+const artworkRouter = Router();
 
-artworkRouter.get('/:artworkId', async (req, res) => {
+artworkRouter.get('/byId/:artworkId', async (req, res) => {
   const { artworkId } = req.params;
   try {
     const artwork = await Artwork.findByPk(artworkId);
@@ -12,8 +12,20 @@ artworkRouter.get('/:artworkId', async (req, res) => {
       res.send(user).status(200);
     }
   } catch (err) {
-    console.error('Failed to GET artwork from db', err);
+    console.error('Failed to GET all artwork', err);
     res.sendStatus(500);
+  }
+})
+
+artworkRouter.get('/byUserId/:userId', async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const artwork = await Artwork.findAll({ where: { userId } });
+    const artworkIds = artwork.map((art: any) => art.id);
+    console.log(artworkIds);
+    res.send(artwork).status(200);
+  } catch (err) {
+    console.error('Failed to GET artwork BY USER ID:', err);
   }
 })
 
