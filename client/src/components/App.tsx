c80e876ffc3bc329a449fcb381b72c9f73e3d64a
Bryan Burnside <bpburnside@gmail.com>
2023-07-01 13:55:20 -0500

    Add lazy loading to App.tsx

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
-import Home from './Home';
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
-import Profile from './Profile';
-import StoryBook from './Stories';
-import Ear from './EarStuff/Instrument'
-import Sculpture from './Sculpture'
-import VisualArt from './Eye/VisualArt';
-import Inbox from './Messages/Inbox';
-import Feed from './Feed';
-import '../styles.css';
+// import Home from './Home';
+// import LoginButton from './LoginButton';
+// import LogoutButton from './LogoutButton';
+// import Profile from './Profile';
+// import StoryBook from './Stories';
+// import Ear from './EarStuff/Instrument'
+// import Sculpture from './Sculpture'
+// import VisualArt from './Eye/VisualArt';
+// import Inbox from './Messages/Inbox';
+// import Feed from './Feed';
+// import '../styles.css';
+
+const Home = lazy(() => import ('./Home'));
+const LoginButton = lazy(() => import ('./LoginButton'));
+const LogoutButton = lazy(() => import ('./LogoutButton'));
+const Profile = lazy(() => import ('./Profile'));
+const StoryBook = lazy(() => import ('./Stories'));
+const Ear = lazy(() => import ('./EarStuff/Instrument'));
+const Sculpture = lazy(() => import ('./Sculpture'));
+const VisualArt = lazy(() => import ('./Eye/VisualArt'));
+const Inbox = lazy(() => import ('./Messages/Inbox'));
+const Feed = lazy(() => import ('./Feed'));
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login/*' element={<LoginButton />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/profile/:userId' element={<Profile />} />
-        <Route path='/logout' element={<LogoutButton />} />
-        <Route path='/stories/:roomId' element={<StoryBook />} />
-        <Route path='/music' element={<Ear />} />
-        <Route path='/visualart/:roomId' element={<VisualArt />} />
-        <Route path='/sculpture/:roomId' element={<Sculpture />} />
-        <Route path='/messages' element={<Inbox />} />
-        <Route path='/feed' element={<Feed />} />
-      </Routes>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login/*' element={<LoginButton />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/:userId' element={<Profile />} />
+          <Route path='/logout' element={<LogoutButton />} />
+          <Route path='/stories/:roomId' element={<StoryBook />} />
+          <Route path='/music' element={<Ear />} />
+          <Route path='/visualart/:roomId' element={<VisualArt />} />
+          <Route path='/sculpture/:roomId' element={<Sculpture />} />
+          <Route path='/messages' element={<Inbox />} />
+          <Route path='/feed' element={<Feed />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
