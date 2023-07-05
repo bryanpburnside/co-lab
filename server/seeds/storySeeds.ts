bb3d5cfc69684ef14bf618991c722cdd10f8fb04
Emmy Bishop <miaui96@gmail.com>
2023-07-05 17:52:18 -0500

    Edit seed to account for artworkId association

--- server/seeds/storySeeds.ts
+++ server/seeds/storySeeds.ts
@@ -1,9 +1,13 @@
-import { Story, Pages } from '../database/index.js';
+import { Story, Pages, Artwork } from '../database/index.js';
 
 
 const createSeedData = async () => {
   try {
+    const userId = 'fakeId';
+    const artwork = await Artwork.create({ type: 'story', userId });
+    const { id: artworkId } = artwork.dataValues;
     const story = await Story.create({
+      artworkId,
       title: 'Taming of the Lion',
       coverImage: 'https://res.cloudinary.com/dhin8tgv1/image/upload/v1687649608/uploads/nmg09penyneghplbikt2.png',
     });
