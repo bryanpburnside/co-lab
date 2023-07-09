aaa1965c368b4dd84167b6b8f5ca6c1f335d64fa
Emmy Bishop <miaui96@gmail.com>
2023-07-09 15:28:01 -0500

    Add route + function to grab original artwork image

--- server/routes/artwork.ts
+++ server/routes/artwork.ts
@@ -17,6 +17,26 @@ artworkRouter.get('/byId/:artworkId', async (req, res) => {
   }
 })
 
+artworkRouter.get('/byId/originalImage/:artworkId', async (req, res) => {
+  const { artworkId } = req.params;
+  try {
+    const artwork = await Artwork.findByPk(artworkId);
+    if (artwork) {
+      const art = await getArtByType(artwork.type, artwork.id);
+      if (art) {
+        res.send(art.content).status(200);
+      } else {
+        res.sendStatus(404);
+      }
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    console.error('Failed to GET original image BY ARTWORK ID', err);
+    res.sendStatus(500);
+  }
+});
+
 artworkRouter.get('/byUserId/:userId', async (req, res) => {
   const { userId } = req.params;
   try {
@@ -28,6 +48,24 @@ artworkRouter.get('/byUserId/:userId', async (req, res) => {
   } catch (err) {
     console.error('Failed to GET artwork BY USER ID:', err);
   }
-})
+});
+
+async function getArtByType(type: string, artworkId?: number) {
+  let model: any;
+  if (!artworkId) {
+    console.log('No artwork ID provided');
+  }
+  switch (type) {
+    case 'visual art':
+      model = VisualArt;
+      break;
+    case 'sculpture':
+      model = Sculpture;
+      break;
+    default:
+      return null;
+  }
+  return model.findOne({ where: { artworkId } });
+}
 
 export default artworkRouter;
