4ceee46c900816fd07b5dea15034a65e6c16fd3b
Bryan Burnside <bpburnside@gmail.com>
2023-07-12 10:36:05 -0500

    Reverted to save commit

--- webpack.config.js
+++ webpack.config.js
@@ -63,38 +63,7 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'co-lab',
-      templateContent: ({ htmlWebpackPlugin }) => `
-      <!DOCTYPE html>
-        <html>
-        <head>
-          <meta charset="utf-8">
-          <title>${htmlWebpackPlugin.options.title}</title>
-          <link rel='icon' type='image/x-icon' href='assets/images/favicon.ico'>
-          <style>
-            * {
-              scrollbar-width: thin;
-              scrollbar-color: #F5C968 transparent;
-            }
-
-            *::-webkit-scrollbar {
-              width: 3px;
-            }
-
-            *::-webkit-scrollbar-track {
-              background: transparent;
-            }
-
-            *::-webkit-scrollbar-thumb {
-              background-color: #F5C968;
-              border-radius: 12px;
-              border: 3px solid transparent;
-            }
-          </style>
-        </head>
-        <body>
-          <div id="app"></div>
-        </body>
-        </html>`,
+      templateContent: ({ htmlWebpackPlugin }) => `<!DOCTYPE html><html><head><meta charset="utf-8"><title>${htmlWebpackPlugin.options.title}</title><link rel='icon' type='image/x-icon' href='assets/images/favicon.ico'></head><body><div id="app"></div></body></html>`,
       filename: 'index.html',
     }),
     new webpack.DefinePlugin({
