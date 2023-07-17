f68345b9e64e0f3981af2c5e5bf4d1f735f2be73
Emmy Bishop <miaui96@gmail.com>
2023-07-17 15:14:12 -0500

    Add styling to close button

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 
 const ModalContainer = styled.div`
   position: fixed;
@@ -14,12 +16,29 @@ const ModalContainer = styled.div`
 `;
 
 const ModalContent = styled.div`
+  position: relative;
   background-color: #3d3983;
   padding: 1rem;
   border: 4px solid white;
   border-radius: 10px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
 `;
+
+const XIcon = styled.div`
+  position: absolute;
+  top: -10%;
+  left: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 2rem;
+  cursor: pointer;
+  z-index: 2;
+  clip-path: circle();
+  background-color: #F06b80;
+`;
+
 
 const Modal = ({ isOpen, onClose, roomId, userId, friendList, sendInvite }) => {
   if (!isOpen) {
@@ -29,6 +48,9 @@ const Modal = ({ isOpen, onClose, roomId, userId, friendList, sendInvite }) => {
   return (
     <ModalContainer>
       <ModalContent>
+        <XIcon onClick={onClose}>
+          <FontAwesomeIcon icon={faCircleXmark} size='lg' />
+        </XIcon>
         <h2>Invite Friends</h2>
         {friendList.length ?
           friendList.map((friend: object, i: number) => <p
@@ -38,7 +60,6 @@ const Modal = ({ isOpen, onClose, roomId, userId, friendList, sendInvite }) => {
           :
           null
         }
-        <button onClick={onClose}>Close</button>
       </ModalContent>
     </ModalContainer>
   );
