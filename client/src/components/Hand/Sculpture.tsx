e040ede41141d1359767ff5fdb6b608e65573e87
Bryan Burnside <bpburnside@gmail.com>
2023-07-20 17:26:07 -0500

    Add invites, collaborator images, and update styles

--- client/src/components/Hand/Sculpture.tsx
+++ client/src/components/Hand/Sculpture.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect, createContext } from "react";
 import { useParams } from 'react-router-dom'
 import { io, Socket } from 'socket.io-client';
 import GenerativeArt from "./GenerativeArt";
+import Modal from '../Modal';
+import axios from 'axios';
 import Peer, { MediaConnection } from 'peerjs';
 import {v4 as generatePeerId} from 'uuid';
 import p5 from 'p5';
@@ -16,8 +18,13 @@ const peers = {};
 
 const Sculpture = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-  const [peerId, setPeerId] = useState('')
+  const [peerId, setPeerId] = useState('');
   const { roomId } = useParams();
+  const [currentCollaborators, setCurrentCollaborators] = useState<Array<Object>>([]);
+  const [userImages, setUserImages] = useState<Array<Object>>([]);
+  const [friendList, setFriendList] = useState<Object[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  let currentUser: string;
 
   useEffect(() => {
     setPeerId(generatePeerId());
@@ -80,23 +87,121 @@ const Sculpture = () => {
       console.log(`User ${userId} joined the room`);
     });
 
-    socket.on('disconnectUser', userId => {
-      if (peers[userId]) {
-        peers[userId].close();
-      }
-    })
+    // socket.on('disconnectUser', userId => {
+    //   if (peers[userId]) {
+    //     peers[userId].close();
+    //   }
+    // })
 
-    // Clean up event listeners
-    return () => {
-      socket.emit('disconnectUser', peerId, roomId);
-      socket.disconnect();
-      peer.disconnect();
-    };
+    // // Clean up event listeners
+    // return () => {
+    //   socket.emit('disconnectUser', peerId, roomId);
+    //   socket.disconnect();
+    //   peer.disconnect();
+    // };
   }, []);
+
+  useEffect(() => {
+    if (user?.sub) {
+      currentUser = user?.sub;
+    }
+    const getUserImageAndSendInfo = async () => {
+      if (user?.sub) {
+        try {
+          const { userId, picture } = await getUserImage(user?.sub);
+          setCurrentCollaborators(prevCollaborators => [...prevCollaborators, { userId, picture }])
+          socket.emit('sendUserInfo', { userId, picture, roomId });
+        } catch (err) {
+          console.error('Failed to GET user image at client:', err);
+        }
+      }
+    };
+
+    getUserImageAndSendInfo();
+
+    socket.on('userInfoReceived', ({ userId, collaborators, roomId }) => {
+      setCurrentCollaborators(collaborators);
+      console.log(collaborators);
+    });
+
+    socket.on('collaboratorDisconnected', (userId, roomId) => {
+      setCurrentCollaborators((prevCollaborators) =>
+        prevCollaborators.filter((collaborator) => collaborator['userId'] !== userId)
+      );
+    });
+
+    return () => {
+      socket.emit('collaboratorDisconnect', currentUser, roomId);
+    };
+  }, [user?.sub]);
+
+  const getUserImage = async (userId: string) => {
+    try {
+      const { data } = await axios.get(`/users/${userId}`);
+      const collaborator = {
+        userId,
+        picture: data.picture
+      }
+      return collaborator;
+    } catch (err) {
+      console.error('Failed to GET user images at client:', err);
+    }
+  }
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const openModal = async () => {
+    try {
+      setIsModalOpen(true);
+      await getFriends();
+    } catch (err) {
+      console.error('Failed to send invite at client:', err);
+    }
+  }
+
+  const getFriends = async () => {
+    try {
+      setIsModalOpen(true);
+      const { data } = await axios.get(`/users/${user?.sub}`);
+      const friends = await Promise.all(
+        data.friends.map(async (friendId: string) => {
+          const userObj = await axios.get(`/users/${friendId}`);
+          return { name: userObj.data.name, id: friendId };
+        }))
+      setFriendList(friends);
+    } catch (err) {
+      console.error('Failed to GET user friends at client:', err);
+    }
+  }
+
+  const sendInvite = async (senderId: string, receiverId: string, message: string) => {
+    socket.emit('directMessage', {
+      senderId,
+      receiverId,
+      message,
+    });
+  }
 
   return (
     <SocketContext.Provider value={socket}>
-      <GenerativeArt roomId={roomId} />
+      <Modal
+          isOpen={isModalOpen}
+          onClose={handleCloseModal}
+          roomId={roomId} userId={user?.sub}
+          friendList={friendList}
+          sendInvite={sendInvite}
+        />
+      <GenerativeArt 
+      roomId={roomId} 
+      openModal={openModal}
+      currentCollaborators={currentCollaborators}
+      />
     </SocketContext.Provider>
   );
 };
