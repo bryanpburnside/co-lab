27ef69db9cbb348b3edad65167a0dbd5abcebc2f
Emmy Bishop <miaui96@gmail.com>
2023-07-20 16:17:19 -0500

    Begin fixing pen width slider styling

--- client/src/components/Eye/Sketch.tsx
+++ client/src/components/Eye/Sketch.tsx
@@ -6,7 +6,9 @@ import axios from 'axios';
 import paper, { Color } from 'paper';
 import styled from 'styled-components';
 import { FriendImage } from '../Profile/Profile';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FaPen, FaPenFancy, FaPalette, FaEraser, FaSave, FaUserPlus } from 'react-icons/fa';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 interface DrawProps {
   backgroundColor: string;
   handleBackgroundColorChange: (color: string) => void;
@@ -31,6 +33,8 @@ const StyledCanvas = styled.canvas<{ backgroundColor: string }>`
 
 const DrawContainer = styled.div`
   position: absolute;
+  display: flex;
+  flex-direction: column;
   top: 50%;
   left: 5%;
   transform: translateY(-50%);
@@ -38,6 +42,43 @@ const DrawContainer = styled.div`
 
 const ColorPicker = styled.input`
   display: none;
+`;
+
+const PenWidthSliderWrapper = styled.div`
+  position: absolute;
+  top: 12.5rem;
+  left: 2rem;
+  transform: translateY(-50%) rotate(90deg);
+  transform-origin: left center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const PenWidthSlider = styled.div`
+  background-color: #3d3983;
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+
+  button[type="button"] {
+    font-size: 6px;
+  }
+`;
+
+const XIcon = styled.div`
+  position: absolute;
+  top: 20%;
+  left: 95%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: flex-start;
+  color: white;
+  font-size: 1.25rem;
+  cursor: pointer;
+  z-index: 2;
 `;
 
 const ButtonContainer = styled.div`
@@ -299,6 +340,29 @@ const Draw: React.FC<DrawProps> = ({ backgroundColor, setBackgroundColor, handle
         />
       )}
       <DrawContainer>
+        <PenWidthSliderWrapper>
+          {showPenWidthSlider && (
+            <PenWidthSlider>
+              <input
+                type="range"
+                value={penWidth.toString()}
+                onChange={handlePenWidthSliderChange}
+                min={1}
+                max={100}
+                className="slider is-small"
+              />
+              {/* <Button
+                  type="button"
+                  onClick={handlePenWidthSliderClose}
+                >
+                  Close
+                </Button> */}
+              <XIcon onClick={handlePenWidthSliderClose}>
+                <FontAwesomeIcon icon={faXmark} />
+              </XIcon>
+            </PenWidthSlider>
+          )}
+        </PenWidthSliderWrapper>
         <ButtonContainer style={{ marginTop: '25rem' }}>
           <ColorPicker
             type="color"
@@ -334,24 +398,6 @@ const Draw: React.FC<DrawProps> = ({ backgroundColor, setBackgroundColor, handle
           >
             <FaPenFancy />
           </Button>
-          {showPenWidthSlider && (
-            <div className="pen-width-slider">
-              <input
-                type="range"
-                value={penWidth.toString()}
-                onChange={handlePenWidthSliderChange}
-                min={1}
-                max={100}
-                className="slider is-small"
-              />
-              <Button
-                type="button"
-                onClick={handlePenWidthSliderClose}
-              >
-                Close
-              </Button>
-            </div>
-          )}
         </ButtonContainer>
         <ButtonContainer>
           <Button
