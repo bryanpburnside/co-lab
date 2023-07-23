79b6238c4401bfaa8c8ad4c81753bd6c21586c1e
Emmy Bishop <miaui96@gmail.com>
2023-07-23 10:58:24 -0500

    Delete unused code

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,18 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
-// import Home from './Home';
-// import LoginButton from './LoginButton';
-// import LogoutButton from './LogoutButton';
-// import Profile from './Profile';
-// import StoryBook from './Stories';
-// import Ear from './EarStuff/Instrument'
-// import Sculpture from './Sculpture'
-// import VisualArt from './Eye/VisualArt';
-// import Inbox from './Messages/Inbox';
-// import Feed from './Feed';
-// import Trimmer from './EarStuff/Trimmer'
-// import '../styles.css';
 
 const Home = lazy(() => import('./Home'));
 const LoginButton = lazy(() => import('./LoginButton'));
